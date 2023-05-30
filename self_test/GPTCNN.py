0ad9145809fa02ae803cbe229517be7ef2607cf4
filip157 <91519640+filip157@users.noreply.github.com>
2023-05-30 19:04:42 +0800

    Update GPTCNN.py

--- self_test/GPTCNN.py
+++ self_test/GPTCNN.py
@@ -36,7 +36,7 @@ X_train = X_train / 255.0
 
 # 建立模型
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(640, 832, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
